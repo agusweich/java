5d5f72c6616a144def067dd1fdbf6c855b14d75d
agustin <agusweich@hotmail.com>
2022-03-04 15:16:40 -0300

    proyecto java

---
+++ clase1.js
@@ -0,0 +1,87 @@
+
+
+
+let botonclick = document.getElementById ("ingresar");
+botonclick.addEventListener ("click", respuesta);
+
+
+
+
+function cargarusuarios(){
+     (fetch ('clase.json'))
+     .then (Response => Response.json())
+     .then (Response=> console.log(Response))
+     .catch (error=>console.log(error))
+}
+    cargarusuarios();
+    
+
+
+
+
+
+
+
+
+
+        
+   
+function respuesta(){
+    
+
+
+    let nombre= document.getElementById ("alumno").value;
+    let materia= document.getElementById ("materia").value;
+    let primerparcial= document.getElementById ("notauno").value;
+    let segundoparcial= document.getElementById ("notados").value;
+    
+
+   
+
+    
+
+    let info = {
+        alumno: nombre,
+        materia: materia,
+        nota1: primerparcial,
+        nota2: segundoparcial,
+        Promedio: (parseInt(primerparcial)+ parseInt(segundoparcial)) /2
+
+    }
+
+
+
+
+    
+    
+    
+
+        swal ('DATOS GUARDADOS', "felicitaciones", "success");
+        console.log (info)
+        localStorage.setItem("informacion alumno", JSON.stringify(info))
+        let informacion = localStorage.getItem ("informacion alumno")
+        console.log(informacion)
+        
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
